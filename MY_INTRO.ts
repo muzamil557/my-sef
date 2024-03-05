69e5c5367cd28ef29198376b6d58ac193ba5826e
muzamil557 <142626022+muzamil557@users.noreply.github.com>
2024-03-05 14:19:06 +0500

    my assighment

---
+++ MY_INTRO.ts
@@ -0,0 +1,16 @@
+let userName : string = "MUZAMIL"
+console.log(userName)
+let fatherName : string = "QURBAN ALI"
+console.log(fatherName)
+let cast : string = "MALIK"
+console.log(cast)
+let age : Number = 21
+console.log(age)
+let contact : Number = 786
+console.log(contact)
+let fee : Number = 0.00
+console.log(fee)
+let isMarried: boolean = false
+console.log(isMarried)
+
+//console.log(userName, fatherName, cast, age, contact, fee, isMarried);
